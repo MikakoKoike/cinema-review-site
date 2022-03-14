6d0e127b49f14caf9392ab6f651af25dc160fa7c
Mika Nakagawara <mika.nakagawara@rakus-partners.co.jp>
2022-03-14 11:15:46 +0900

    document作成

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <h1>タイトル</h1>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+<script>
+import { Component, Vue } from "vue-property-decorator";
+import M from "materialize-css/dist/js/materialize.min";
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@Component({
+  components: {},
+})
+export default class App extends Vue {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  mounted() {
+    //cdnのインストールが必要。mountedだとタイミングが合わないので、時間をずらした。
+    setTimeout(() => {
+      M.AutoInit();
+    }, 100);
+  }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
