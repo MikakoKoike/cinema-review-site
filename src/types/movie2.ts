780e1dca02b448c40f92faa161becac44fe3b78d
ryota-09 <n.ryo950911@gmail.com>
2022-03-15 11:58:12 +0900

    修正

---
+++ src/types/movie2.ts
@@ -0,0 +1,15 @@
+import { Movie } from "./movie";
+
+export class Movie2 extends Movie {
+  private reviewList
+}
+
+// +id:number
+// +titile:string
+// +country:string
+// +genre:string
+// +actors:string
+// +reviewList<Review>
+// +countLike:number
+// +createdYear:number
+// +releasedDate:date
---
+++ src/types/review.ts
@@ -0,0 +1,4 @@
+export class Review {
+  private _id: number;
+  private
+}
